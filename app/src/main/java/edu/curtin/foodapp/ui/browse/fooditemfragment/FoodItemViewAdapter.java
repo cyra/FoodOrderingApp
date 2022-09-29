dcddc7cf36fc9abed145e757530c8d18a1700030
vevilo <zak.a.booth@gmail.com>
2022-09-30 01:27:24 +0800

    Add item to cart from Browse

--- app/src/main/java/edu/curtin/foodapp/ui/browse/fooditemfragment/FoodItemViewAdapter.java
+++ app/src/main/java/edu/curtin/foodapp/ui/browse/fooditemfragment/FoodItemViewAdapter.java
@@ -32,7 +32,10 @@ import java.util.ArrayList;
 
 import edu.curtin.foodapp.R;
 import edu.curtin.foodapp.databinding.SingleFoodBinding;
+import edu.curtin.foodapp.model.cart.CartItem;
+import edu.curtin.foodapp.model.cart.CartItemList;
 import edu.curtin.foodapp.model.fooditems.FoodItem;
+import edu.curtin.foodapp.model.fooditems.FoodItemList;
 import edu.curtin.foodapp.ui.browse.BrowseFragment;
 import edu.curtin.foodapp.ui.cart.cartfragment.CartFragment;
 import edu.curtin.foodapp.ui.cart.cartfragment.CartListFragment;
@@ -40,11 +43,16 @@ import edu.curtin.foodapp.ui.cart.cartfragment.CartListFragment;
 public class FoodItemViewAdapter extends RecyclerView.Adapter<FoodItemViewHolder> {
     private final Context context;
     private SingleFoodBinding binding;
+
     private final ArrayList<FoodItem> foodItems;
+    private CartItemList cart;
 
     public FoodItemViewAdapter(Context context, ArrayList<FoodItem> foodItems) {
         this.context = context;
         this.foodItems = foodItems;
+
+        this.cart = new CartItemList();
+        cart.load(context);
     }
 
     @NonNull
@@ -56,18 +64,36 @@ public class FoodItemViewAdapter extends RecyclerView.Adapter<FoodItemViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull FoodItemViewHolder holder, int position) {
-        holder.itemName.setText(foodItems.get(position).getName());
-        holder.itemDescription.setText(foodItems.get(position).getDescription());
-        holder.itemPrice.setText(String.valueOf(foodItems.get(position).getPrice()));
-        // If empty show placeholder
-        if (!foodItems.get(position).getImg().isEmpty()){
-        holder.itemImg.setImageResource(getImage(foodItems.get(position).getImg()));
+        FoodItem item = foodItems.get(position);
+
+        holder.itemName.setText(item.getName());
+        holder.itemDescription.setText(item.getDescription());
+        holder.itemPrice.setText(String.valueOf(item.getPrice()));
+        if (!item.getImg().isEmpty()) {
+            holder.itemImg.setImageResource(getImage(item.getImg()));
+        }
+        else {
+            // Show placeholder image
         }
 
-        // onclick item opens cart fragment
+        // Clicking item opens Cart
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int id = item.getID();
+                double price = item.getPrice();
+                CartItem item;
+
+                if (cart.getCartItemByID(id) == null) {
+                    item = new CartItem(id, price);
+                    cart.addCartItem(item);
+                } else {
+                    // If item already exists in cart, increase quantity
+                    item = cart.getCartItemByID(id);
+                    item.increaseQuantity();
+                    cart.editCartItem(item);
+                }
+
                 Navigation.findNavController(view).navigate(R.id.action_navigation_browse_to_navigation_cart);
             }
         });
